a71e210e502f44fb33e28a4a527530a17eefee24
Eren Yagdiran <erenyagdiran@gmail.com>
2015-01-29 10:37:33 -0500

    Start & Stop scripts are added

--- vct_controller_dummy/vct_init.sh
+++ vct_controller_dummy/vct_init.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-exec /sbin/setuser vct sudo service libvirt-bin start && sudo service postfix start && exec "/home/vct/confine-dist/utils/vct/vct_system_init"
+exec /sbin/setuser vct sudo service libvirt-bin start && sudo service postfix start && sudo service rabbitmq-server restart && exec "/home/vct/confine-dist/utils/vct/vct_system_init"
